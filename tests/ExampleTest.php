33edbffeaece4118492c8e26684233002243ee9f
Tatu Kulmala <tatu@euronic.fi>
2024-09-24 12:36:44 +0300

    PHPUnit
    - Create & define configuration file for PHPUnit
    - Create Example Test file

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Seravo\Tests\SeravoApi;
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase
+{
+    public function testExampleTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
