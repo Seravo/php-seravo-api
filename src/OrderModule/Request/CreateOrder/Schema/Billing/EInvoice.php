ba4924be0880eff328cb8e245eef9d56a41c479b
Tatu Kulmala <tatu@euronic.fi>
2024-10-04 12:18:35 +0300

    Implement schema classes for CreateOrderRequest

---
+++ src/OrderModule/Request/CreateOrder/Schema/Billing/EInvoice.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Seravo\SeravoApi\OrderModule\Request\CreateOrder\Schema\Billing;
+
+class EInvoice extends BillingMethod
+{
+    public const BILLING_METHOD = 'einvoice';
+
+    public function __construct(
+        public string $contactEmail,
+        public string $contactName,
+        public string $contactPhone,
+        public string $invoice,
+        public string $operator,
+        public ?string $address = null,
+        public ?string $address2 = null,
+        public ?string $city = null,
+        public ?string $email = null,
+        public ?string $name = null,
+        public ?string $postal = null
+    ) {
+        parent::__construct($contactEmail, $contactName, $contactPhone, self::BILLING_METHOD);
+    }
+}
