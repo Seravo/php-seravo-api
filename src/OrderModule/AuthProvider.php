5436cdfd6ff963a1da99eae84b6402dd5932ab52
Tatu Kulmala <tatu@euronic.fi>
2024-10-04 16:37:53 +0300

    Create AuthProvider class

---
+++ src/OrderModule/AuthProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Seravo\SeravoApi\OrderModule;
+
+use InvalidArgumentException;
+use Jumbojett\OpenIDConnectClient;
+
+class AuthProvider
+{
+    private string $accessToken;
+
+    private const TOKEN_ENDPOINT = 'https://idp.seravo.dev/realms/Seravo/protocol/openid-connect/token';
+
+    public function __construct(string $clientId, string $secret)
+    {
+        $oidc = new OpenIDConnectClient('https://idp.seravo.dev', $clientId, $secret);
+        $oidc->providerConfigParam(['token_endpoint' => self::TOKEN_ENDPOINT]);
+        $oidc->addScope(['openid']);
+
+        $clientCredentialsToken = $oidc->requestClientCredentialsToken()->access_token;
+
+        // TODO: Check for valid JWT
+        if (!$clientCredentialsToken) {
+            throw new InvalidArgumentException('Invalid Access Token');
+        }
+
+        $this->accessToken = $clientCredentialsToken;
+    }
+
+    public function getAccessToken(): string
+    {
+        return $this->accessToken;
+    }
+}
