d6f6c18e4cd11ec03fd58f095263a68bdbad8f59
Tatu Kulmala <tatu@euronic.fi>
2024-11-21 14:30:59 +0200

    Replace hardcoded string definitions of modules and endpoints with corresponding backed enum classes

--- src/Apis/OrderApi.php
+++ src/Apis/OrderApi.php
@@ -4,18 +4,17 @@ declare(strict_types=1);
 
 namespace Seravo\SeravoApi\Apis;
 
+use Seravo\SeravoApi\Enums\ApiModule;
 use Seravo\SeravoApi\HttpClient\Builder;
 use Seravo\SeravoApi\Apis\Order\Endpoint\Orders;
 
 class OrderApi extends AbstractApi
 {
-    public const ENDPOINT_PREFIX = '/order/';
-
     public function __construct(
         private readonly string $baseUrl,
         private readonly Builder $httpClientBuilder
     ) {
-        parent::__construct($this->baseUrl, $this->httpClientBuilder, self::ENDPOINT_PREFIX);
+        parent::__construct($this->baseUrl, $this->httpClientBuilder, ApiModule::Order);
     }
 
     public function orders(): Orders
