f6b49e8d86d0b12ec5a418cb203815036e38c482
Tatu Kulmala <tatu@euronic.fi>
2024-11-21 14:30:59 +0200

    Replace hardcoded string definitions of modules and endpoints with corresponding backed enum classes

--- src/Apis/PublicApi.php
+++ src/Apis/PublicApi.php
@@ -4,16 +4,15 @@ declare(strict_types=1);
 
 namespace Seravo\SeravoApi\Apis;
 
+use Seravo\SeravoApi\Enums\ApiModule;
 use Seravo\SeravoApi\HttpClient\Builder;
 
 class PublicApi extends AbstractApi
 {
-    public const ENDPOINT_PREFIX = '/public/';
-
     public function __construct(
         private readonly string $baseUrl,
         private readonly Builder $httpClientBuilder
     ) {
-        parent::__construct($this->baseUrl, $this->httpClientBuilder, self::ENDPOINT_PREFIX);
+        parent::__construct($this->baseUrl, $this->httpClientBuilder, ApiModule::Public);
     }
 }
