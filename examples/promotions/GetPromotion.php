244d564a82e445cf37907df578a784dbc1facbb2
Tatu Kulmala <tatu@seravo.com>
2025-01-14 12:21:13 +0200

    Create examples for Promotions

---
+++ examples/promotions/GetPromotion.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use Seravo\SeravoApi\SeravoAPI;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(dirname(__DIR__)));
+$dotenv->load();
+
+$api = new SeravoAPI(
+    baseUrl: $_ENV['SERAVO_API_URL'],
+    clientId: $_ENV['SERAVO_API_CLIENT_ID'],
+    secret: $_ENV['SERAVO_API_SECRET']
+);
+
+$api->authenticate(
+    authProviderUrl: $_ENV['SERAVO_KEYCLOAK_PROVIDER_URL'],
+    tokenEndpoint: $_ENV['SERAVO_KEYCLOAK_TOKEN_ENDPOINT_URL']
+);
+
+try {
+    $promotion = $api->order->promotions()->getById(id: '0d83c053-bf27-44c2-83ac-7f7bbe27e61b');
+    dd($promotion);
+} catch (Exception $exception) {
+    dd($exception);
+}
