0807366a739fb513c2757cec6bac7d03b074e627
Tatu Kulmala <tatu@seravo.com>
2025-01-15 15:47:25 +0200

    Create examples for AdditionalServices

---
+++ examples/additional_services/GetAdditionalService.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use Seravo\SeravoApi\SeravoAPI;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(dirname(__DIR__)));
+$dotenv->load();
+
+$api = new SeravoAPI(
+    baseUrl: $_ENV['SERAVO_API_URL'],
+    clientId: $_ENV['SERAVO_API_CLIENT_ID'],
+    secret: $_ENV['SERAVO_API_SECRET']
+);
+
+$api->authenticate(
+    authProviderUrl: $_ENV['SERAVO_KEYCLOAK_PROVIDER_URL'],
+    tokenEndpoint: $_ENV['SERAVO_KEYCLOAK_TOKEN_ENDPOINT_URL']
+);
+
+try {
+    $additionalService = $api->order->additionalServices()->getById(id: 'ae921402-fe1a-4fce-815b-86a411051ad9');
+    dd($additionalService);
+} catch (Exception $exception) {
+    dd($exception);
+}
