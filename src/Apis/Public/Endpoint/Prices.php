75675d43a87831417ccbceab31c0f0b221db439a
Tatu Kulmala <tatu@seravo.com>
2025-01-29 12:17:03 +0200

    Refactor API endpoints to use response classes and refactor response handling

--- src/Apis/Public/Endpoint/Prices.php
+++ src/Apis/Public/Endpoint/Prices.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Seravo\SeravoApi\Apis\Public\Endpoint;
 
-use Seravo\SeravoApi\Apis\Public\Request\Price\CreatePriceRequest;
 use Seravo\SeravoApi\Apis\PublicApi;
+use Seravo\SeravoApi\Apis\Public\Request\Price\CreatePriceRequest;
+use Seravo\SeravoApi\Apis\Public\Response\Price;
 use Seravo\SeravoApi\Enums\HttpMethod;
 use Seravo\SeravoApi\Enums\ApiEndpoint;
 
@@ -22,23 +23,31 @@ class Prices
     /**
      * Create a new Price
      * @see API Reference: https://api.seravo.com/public/docs#/Prices/create_public_prices__post
-     *
-     * @return array<string, mixed>
+     * @param CreatePriceRequest $request
+     * @return Price
      */
-    public function create(CreatePriceRequest $request): array
+    public function create(CreatePriceRequest $request): Price
     {
-        return $this->api->request(method: HttpMethod::Post, uri: $this->uri, body: $request);
+        return $this->api->request(
+            method: HttpMethod::Post,
+            uri: $this->uri,
+            body: $request,
+            responseClass: Price::class
+        );
     }
 
     /**
      * Return a single Price
      * @see API Reference: https://api.seravo.dev/public/docs#/Prices/get_one_public_prices__id__get
-     *
-     * @param string $id - Uuid
-     * @return array<mixed, mixed>
+     * @param string $id - UUID
+     * @return Price
      */
-    public function getById(string $id): array
+    public function getById(string $id): Price
     {
-        return $this->api->request(method: HttpMethod::Get, uri: $this->uri . $id);
+        return $this->api->request(
+            method: HttpMethod::Get,
+            uri: $this->uri . $id,
+            responseClass: Price::class
+        );
     }
 }
