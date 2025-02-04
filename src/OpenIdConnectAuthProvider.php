5cc4d1db6875282f3ba6ed00d3227cebd110c295
Tatu Kulmala <tatu@seravo.com>
2025-02-04 16:42:55 +0200

    Implement `TokenVerifier` plugin

--- src/OpenIdConnectAuthProvider.php
+++ src/OpenIdConnectAuthProvider.php
@@ -12,18 +12,25 @@ class OpenIdConnectAuthProvider implements AuthProviderInterface
 {
     private string $accessToken;
 
-    public function __construct(string $clientId, string $secret, string $providerUrl)
-    {
-        $oidc = new OpenIDConnectClient($providerUrl, $clientId, $secret);
-        $oidc->addScope(['openid']);
+    public function __construct(
+        private readonly string $clientId,
+        private readonly string $secret,
+        private readonly string $providerUrl,
+    ) {
+        try {
+            $oidc = new OpenIDConnectClient($this->providerUrl, $this->clientId, $this->secret);
+            $oidc->addScope(['openid']);
 
-        $accessToken = $oidc->requestClientCredentialsToken()->access_token ?? null;
+            $accessToken = $oidc->requestClientCredentialsToken()->access_token ?? null;
 
-        if (!$accessToken) {
-            throw new InvalidAccessTokenException();
+            if (!$accessToken) {
+                throw new InvalidAccessTokenException('Invalid access token');
+            }
+
+            $this->accessToken = $accessToken;
+        } catch (InvalidAccessTokenException $e) {
+            throw new InvalidAccessTokenException($e->getMessage());
         }
-
-        $this->accessToken = $accessToken;
     }
 
     public function getAccessToken(): string
