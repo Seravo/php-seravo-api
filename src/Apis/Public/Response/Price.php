9e891d0f0554b4e476e576d006a5eb1dff24815b
Tatu Kulmala <tatu@seravo.com>
2026-01-16 15:28:27 +0200

    Extend Price response class to support plan and domain objects
    Impact: major
    Related: CP-359

--- src/Apis/Public/Response/Price.php
+++ src/Apis/Public/Response/Price.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Seravo\SeravoApi\Apis\Public\Response;
 
 use Seravo\SeravoApi\Apis\AbstractResponse;
+use Seravo\SeravoApi\Apis\Public\Response\PricePlan;
 
 readonly class Price extends AbstractResponse
 {
     /**
+     * @param PriceDomain[] $domains
      * @param PriceProduct[] $products
      */
     public function __construct(
@@ -16,6 +18,8 @@ readonly class Price extends AbstractResponse
         public int $interval,
         public string $total,
         public array $products,
+        public PricePlan $plan,
+        public array $domains,
         public ?\DateTime $createdAt,
         public ?\DateTime $updatedAt = null,
         public ?string $promotion = null
