29fa8da73ff705fb632e427c497255ad7e058711
Tatu Kulmala <tatu@seravo.com>
2026-05-12 14:43:13 +0300

    Accept customizable query parameters in GET affiliates endpoint
    This commit adds support for additional query parameters in the GET
    affiliates endpoint, allowing clients to specify custom
    filters and sorting options when retrieving affiliate data.
    
    Impact: patch
    Related: CP-359

--- src/Apis/Order/Endpoint/Affiliates.php
+++ src/Apis/Order/Endpoint/Affiliates.php
@@ -6,6 +6,7 @@ namespace Seravo\SeravoApi\Apis\Order\Endpoint;
 
 use Seravo\SeravoApi\Apis\OrderApi;
 use Seravo\SeravoApi\Enums\ApiEndpoint;
+use Seravo\SeravoApi\Enums\SortDirection;
 use Seravo\SeravoApi\Apis\Order\Response\Affiliate;
 use Seravo\SeravoApi\Apis\Order\Response\AffiliateCollection;
 
@@ -20,13 +21,36 @@ class Affiliates
     }
 
     /**
-     * Return all Affiliates
+     * Return Affiliates
      * @see API Reference: https://api.seravo.com/order/docs#/Affiliates/get_many_order_affiliates__get
+     *
+     * @param int                 $page    Page number (defaults to 1)
+     * @param int                 $limit   Number of items per page; 0 disables paging
+     * @param string|null         $name    Optional name filter
+     * @param string|null         $partner_id Optional partner_id filter
+     * @param array<string, SortDirection>  $sort    Optional sort fields
      * @return AffiliateCollection
      */
-    public function get(): AffiliateCollection
-    {
-        return $this->api->get(uri: $this->uri . '?limit=0', responseClass: AffiliateCollection::class);
+    public function get(
+        int $page = 1,
+        int $limit = 0,
+        ?string $name = null,
+        ?string $partner_id = null,
+        array $sort = []
+    ): AffiliateCollection {
+        $query = [
+            'page' => $page,
+            'limit' => $limit,
+        ];
+        if ($name !== null) {
+            $query['name'] = $name;
+        }
+        if ($partner_id !== null) {
+            $query['partner_id'] = $partner_id;
+        }
+
+        $uri = $this->api->buildUriWithParams($this->uri, $query, $sort);
+        return $this->api->get(uri: $uri, responseClass: AffiliateCollection::class);
     }
 
     /**
---
+++ tests/Endpoints/AffiliatesEndpointUriTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Seravo\Tests\SeravoApi\Endpoints;
+
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Utils;
+use PHPUnit\Framework\TestCase;
+use Seravo\SeravoApi\Enums\SortDirection;
+
+class AffiliatesEndpointUriTest extends TestCase
+{
+    public function testGetAffiliatesUriQuery(): void
+    {
+        $history = [];
+        $historyMiddleware = Middleware::history($history);
+
+        $mock = new \GuzzleHttp\Handler\MockHandler([
+            new Response(200, ['Content-Type' => 'application/json'], Utils::streamFor('{"results":[]}')),
+        ]);
+        $handlerStack = HandlerStack::create($mock);
+        $handlerStack->push($historyMiddleware);
+
+        $client = new \Seravo\SeravoApi\SeravoAPI(
+            'test1234',
+            'test1234',
+            null,
+            new \Seravo\SeravoApi\HttpClient\Builder(
+                new \GuzzleHttp\Client(['handler' => $handlerStack])
+            )
+        );
+
+        // Test with all parameters
+        $page = 2;
+        $limit = 10;
+        $name = 'test';
+        $partner_id = 'partner123';
+        $sort = ['name' => SortDirection::Asc];
+        $client->order->affiliates()->get($page, $limit, $name, $partner_id, $sort);
+
+        $this->assertNotEmpty($history, 'No request was recorded');
+        $request = $history[0]['request'];
+        $uri = (string)$request->getUri();
+
+        $this->assertStringContainsString('page=2', $uri);
+        $this->assertStringContainsString('limit=10', $uri);
+        $this->assertStringContainsString('name=test', $uri);
+        $this->assertStringContainsString('partner_id=partner123', $uri);
+        $this->assertStringContainsString('sort=name%3Aasc', $uri);
+    }
+}
