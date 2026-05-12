106ee436e4551e66425c191c024b08c5278402ac
Tatu Kulmala <tatu@seravo.com>
2026-05-12 14:54:20 +0300

    Accept customizable query parameters in GET promotions endpoint
    This commit adds support for additional query parameters in the GET
    promotions endpoint, allowing clients to specify custom
    filters and sorting options when retrieving promotion data.
    
    Impact: patch
    Related: CP-359

--- src/Apis/Order/Endpoint/Promotions.php
+++ src/Apis/Order/Endpoint/Promotions.php
@@ -6,6 +6,7 @@ namespace Seravo\SeravoApi\Apis\Order\Endpoint;
 
 use Seravo\SeravoApi\Apis\OrderApi;
 use Seravo\SeravoApi\Enums\ApiEndpoint;
+use Seravo\SeravoApi\Enums\SortDirection;
 use Seravo\SeravoApi\Apis\Order\Response\PromotionCode;
 use Seravo\SeravoApi\Apis\Order\Response\PromotionCodeCollection;
 
@@ -20,13 +21,32 @@ class Promotions
     }
 
     /**
-     * Return all PromotionCodes
+     * Return PromotionCodes
      * @see API Reference: https://api.seravo.com/order/docs#/Promotions/get_many_order_promotions__get
+     *
+     * @param int                 $page    Page number (defaults to 1)
+     * @param int                 $limit   Number of items per page; 0 disables paging
+     * @param string|null         $name    Optional name filter
+     * @param array<string, SortDirection>  $sort    Optional sort fields
      * @return PromotionCodeCollection
      */
-    public function get(): PromotionCodeCollection
-    {
-        return $this->api->get(uri: $this->uri . '?limit=0', responseClass: PromotionCodeCollection::class);
+    public function get(
+        int $page = 1,
+        int $limit = 0,
+        ?string $name = null,
+        array $sort = []
+    ): PromotionCodeCollection {
+        $query = [
+            'page' => $page,
+            'limit' => $limit,
+        ];
+
+        if ($name !== null) {
+            $query['name'] = $name;
+        }
+
+        $uri = $this->api->buildUriWithParams(baseUri: $this->uri, query: $query, sort: $sort);
+        return $this->api->get(uri: $uri, responseClass: PromotionCodeCollection::class);
     }
 
     /**
