aab340bdcf2c277b0f105499143adb9f4b4d141f
Tatu Kulmala <tatu@seravo.com>
2026-05-21 10:36:51 +0300

    Fix PHP 8.4 deprecation warnings
    This commit updates the constructor of the Builder class to explicitly
    declare the parameters as nullable.
    
    This change is necessary to avoid deprecation warnings in PHP 8.4, where
    implicitly nullable parameters are deprecated. By adding the '?' before
    the type declarations, we ensure that the code is compatible with
    PHP 8.4 and future versions.
    
    Impact: major
    Related: CP-359

--- src/Exceptions/InvalidAccessTokenException.php
+++ src/Exceptions/InvalidAccessTokenException.php
@@ -6,8 +6,11 @@ namespace Seravo\SeravoApi\Exceptions;
 
 class InvalidAccessTokenException extends \Exception
 {
-    public function __construct(string $message = 'Invalid Access Token')
-    {
-        parent::__construct($message);
+    public function __construct(
+        string $message = 'Invalid Access Token',
+        int $code = 0,
+        ?\Throwable $previous = null
+    ) {
+        parent::__construct($message, $code, $previous);
     }
 }
